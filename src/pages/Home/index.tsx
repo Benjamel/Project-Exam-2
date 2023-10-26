678d52d9d71fdc380392e04bf7e3170112071761
Benjamel <benjamin.meldal@gmail.com>
2023-10-26 14:14:29 +0200

    home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Venue from '../../types';
-import fetchVenues from '../../services/products';
+import fetchVenues from '../../services/venues';
+import Search from '../../components/Search';
+import * as S from '../../App.styles';
 
 function Home() {
   const [venues, setVenues] = useState<Venue[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+
   useEffect(() => {
     fetchVenues()
       .then((data) => {
         setVenues(data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching venues', error);
+        setIsLoading(false);
       });
   }, []);
 
+  const filteredVenues = venues.filter((venue) =>
+    venue.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  if (isLoading) {
+    return <div className='text-center'>Venues Loading..</div>;
+  }
+
   return (
-    <div className='text-white'>
-      <h1>This is home</h1>
-      <ul>
-        {venues.map((venue) => (
-          <li key={venue.id}>
-            <Link className='text-white hover:text-white' to={`/venue/${venue.id}`}>
-              {venue.name}
-            </Link>
-          </li>
+    <div>
+      <div className='text-center'>
+        <h1 className='text-center mt-4'>Venues</h1>
+        <Search searchQuery={searchQuery} onSearch={setSearchQuery} />
+      </div>
+      <S.ProductCard>
+        {filteredVenues.map((venue) => (
+          <Link to={`/venue/${venue.id}`} key={venue.id}>
+            <img src={venue.media[0]} alt={venue.name} />
+            <h2>{venue.name}</h2>
+          </Link>
         ))}
-      </ul>
+      </S.ProductCard>
     </div>
   );
 }
