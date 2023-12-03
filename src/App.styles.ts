a4e930e2311024635640204a6adae249bbc3ae9a
Benjamel <benjamin.meldal@gmail.com>
2023-12-03 17:21:53 +0100

    login/register styling

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -89,3 +89,121 @@ export const singleVenue = styled.div`
     }
   }
 `;
+
+//Login page
+export const loginForm = styled.div`
+  .login-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  .login-form {
+    width: 300px;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+  }
+
+  .form-group {
+    margin-bottom: 15px;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+
+  input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+  }
+
+  .error-message {
+    color: red;
+    margin-top: 5px;
+  }
+
+  button {
+    background-color: #007bff;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .register-link {
+    background-color: #ccc;
+  }
+
+  .register-link a {
+    color: black;
+    text-decoration: none;
+  }
+
+  .button-group {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+//Register
+export const registerForm = styled.div`
+  .register-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  .register-form {
+    width: 300px;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+  }
+
+  .form-group {
+    margin-bottom: 15px;
+  }
+
+  input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+  }
+
+  .error-message {
+    color: red;
+    margin-top: 5px;
+  }
+
+  button {
+    background-color: #007bff;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-right: 10px;
+  }
+
+  .back-button {
+    background-color: #ccc;
+  }
+
+  .back-button a {
+    color: black;
+    text-decoration: none;
+  }
+
+  .button-group {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
