8ea156006b5415b16c152e07c9c2650cf1bd9ed3
Benjamel <benjamin.meldal@gmail.com>
2023-12-03 10:57:53 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import PageNotFound from './pages/PageNotFound';
+import * as storage from './storage/index';
 
 function App() {
   const location = useLocation();
@@ -14,12 +15,14 @@ function App() {
     document.title = 'Holidaze';
   }
 
+  const accessToken = storage.load('accessToken') as string;
+
   return (
     <div>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='/venue/:venueId' element={<Venue />} />
+          <Route path='/venue/:venueId' element={<Venue accessToken={accessToken} />} />
           <Route path='/home' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
+window.addEventListener('unhandledrejection', (event) => {
+  console.error('Unhandled Promise Rejection:', event.reason);
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
